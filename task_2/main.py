9c9d429d8d10d637f331de56c54834badfaa6ab9
ордей абуз <gordlab@mail.ru>
2025-05-12 20:07:03 +1000

    improves functions with numpy

--- task_2/main.py
+++ task_2/main.py
@@ -14,45 +14,36 @@ class LogisticRecursion(ABC):
         point = numpy.zeros(len(x_param.columns))
 
         for _ in range(iters):
-            res = LogisticRecursion.gradient(x_param=x_param, y_param=y_param, weights=point)
+            res = LogisticRecursion.gradient(x_param=x_param, y_param=y_param.to_numpy(), weights=point)
             point = [point[i_weight] - learning_rate * i_gradient for i_weight, i_gradient in enumerate(res)]
 
         return {'koefs': {i: point[j] for j, i in enumerate(x_param.columns)},
-                'likelihood': LogisticRecursion.likelihood(x_param=x_param, y_param=y_param, weights=point),
-                'log_loss': LogisticRecursion.log_loss(x_param=x_param, y_param=y_param, weights=point)}
+                'likelihood': LogisticRecursion.likelihood(x_param=x_param, y_param=y_param.to_numpy(), weights=point),
+                'log_loss': LogisticRecursion.log_loss(x_param=x_param, y_param=y_param.to_numpy(), weights=point)}
 
     @staticmethod
-    def likelihood(x_param: pandas.DataFrame, y_param: pandas.Series, weights: list):
-        res = 0
-
-        for i in x_param.index:
-            a_i = sum(x_param.loc[i, slice(None)] * weights)
-            p_i = (1 / (1 + numpy.exp(-a_i)))
-            res += (y_param[i] * numpy.log(p_i) + (1 - y_param[i]) * numpy.log((1 - p_i)))
+    def likelihood(x_param: pandas.DataFrame, y_param: numpy.array, weights: list):
+        scores = numpy.dot(x_param, weights)
+        probabilities = 1 / (1 + numpy.exp(-scores))
+        res = sum(y_param * numpy.log(probabilities) + (1 - y_param) * numpy.log(1 - probabilities))
 
         return res / len(x_param.index)
 
     @staticmethod
-    def log_loss(x_param: pandas.DataFrame, y_param: pandas.Series, weights: list):
-        res = 0
-
-        for i in x_param.index:
-            a_i = sum(x_param.loc[i, slice(None)] * weights)
-            p_i = (1 / (1 + numpy.exp(-a_i)))
-            res -= (y_param[i] * numpy.log(p_i) + (1 - y_param[i]) * numpy.log((1 - p_i)))
+    def log_loss(x_param: pandas.DataFrame, y_param: numpy.array, weights: list):
+        scores = numpy.dot(x_param, weights)
+        probabilities = 1 / (1 + numpy.exp(-scores))
+        res = sum(-1 * (y_param * numpy.log(probabilities) + (1 - y_param) * numpy.log(1 - probabilities)))
 
         return res / len(x_param.index)
 
     @staticmethod
-    def gradient(x_param: pandas.DataFrame, y_param: pandas.Series, weights):
-        res = pd.Series(data=[0 for _ in range(len(x_param.columns))], index=[i for i in x_param.columns])
+    def gradient(x_param: pandas.DataFrame, y_param: numpy.array, weights):
+        scores = numpy.dot(x_param, weights)
+        probabilities = 1 / (1 + numpy.exp(-scores))
+        res = numpy.dot((probabilities - y_param), x_param)
 
-        for i in x_param.index:
-            a_i = sum(x_param.loc[i, slice(None)] * weights)
-            p_i = (1 / (1 + numpy.exp(-a_i)))
-            res += (p_i - y_param[i]) * x_param.loc[i]
-
-        return res / len(x_param.index)
+        return pd.Series(res / len(x_param.index), index=x_param.columns)
 
 
 dataset = sklearn.datasets.load_iris(as_frame=True).frame
