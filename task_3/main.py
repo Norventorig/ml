28d8b7172919e330ca9910b3e3fd01b67965d214
ордей абуз <gordlab@mail.ru>
2025-05-15 19:24:23 +1000

    deletes unnecessary data preparing; creates log model

--- task_3/main.py
+++ task_3/main.py
@@ -1,17 +1,24 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
 
 
+scaler = StandardScaler()
 dataset = pd.read_csv('dataset.csv')
 
-dataset['nationality'] = dataset['nationality'].astype('category')
+dataset = dataset[['height', 'weight', 'sex']]
+dataset = dataset.dropna()
 
-dataset['sex'] = dataset['sex'] == 'male'
-dataset['sex'] = dataset['sex'].astype(bool)
+dataset['sex'] = (dataset['sex'] == 'male').astype(int)
 
-dataset['dob'] = pd.to_datetime(dataset['dob'], format='%m/%d/%y')
+x = dataset[['height', 'weight']]
+y = dataset['sex']
 
-dataset['sport'] = dataset['sport'].astype('category')
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=7)
 
-dataset['gold'] = dataset['gold'].astype(bool)
-dataset['silver'] = dataset['silver'].astype(bool)
-dataset['bronze'] = dataset['bronze'].astype(bool)
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+
+model = LogisticRegression()
+model.fit(x_train, y_train)
