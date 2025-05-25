91de8504a873ca49486ff7257be20fdc70c2c3a4
ордей абуз <gordlab@mail.ru>
2025-05-25 15:59:26 +1000

    unprepared metricks

---
+++ task_4/main.py
@@ -0,0 +1,51 @@
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+
+
+pd.set_option('display.max_rows', None)  # Все строки
+pd.set_option('display.max_columns', None)  # Все столбцы
+pd.set_option('display.width', None)  # Автоперенос отключен
+pd.set_option('display.max_colwidth', None)  # Полное содержимое ячеек
+
+
+train_dataset = pd.read_csv('train.csv')
+test_dataset = pd.read_csv('test.csv')
+test_true = pd.read_csv('test_true.csv')
+
+train_dataset.dropna(inplace=True)
+train_dataset.drop(['Embarked', 'Pclass', 'Sex', 'Name', 'Ticket', 'Cabin'], axis=1, inplace=True)
+
+test_dataset.dropna(inplace=True)
+test_dataset.drop(['Embarked', 'Pclass', 'Sex', 'Name', 'Ticket', 'Cabin'], axis=1, inplace=True)
+
+test_true = test_true.merge(test_dataset['PassengerId'], how='inner', on='PassengerId')
+
+x_unprepared_train = train_dataset.drop('Survived', axis=1)
+x_unprepared_test = test_dataset
+
+y = train_dataset['Survived']
+
+model = LogisticRegression()
+model.fit(x_unprepared_train, y)
+
+predictions = model.predict(x_unprepared_test)
+
+# Я решил бахнуть ВСЕ известные мне метрики, кроме log loss,
+# Так как он учитывает уверенность модели, а уверенностью там и не пахнет
+
+accuracy = accuracy_score(test_true['Survived'], predictions)
+precision = precision_score(test_true['Survived'], predictions)
+recall = recall_score(test_true['Survived'], predictions)
+f1 = f1_score(test_true['Survived'], predictions)
+
+print('1 - выжил, 0 - погиб')
+print(f"accuracy: {accuracy}")
+print(f"precision: {precision}")
+print(f"recall: {recall}")
+print(f"f1: {f1}")
+
+# recall 1 потому что модель ни разу не предсказала 0 - смерть
