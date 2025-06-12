4c8d425798a35d9d7ea86d28cf67a4a6e232d615
ордей абуз <gordlab@mail.ru>
2025-06-12 18:26:51 +1000

    too lazy

--- task_4/main.py
+++ task_4/main.py
@@ -6,18 +6,19 @@ from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
+from sklearn.model_selection import train_test_split
 
 
-def model_operation(x, y, true_res, test_x):
+def model_operation(train_x, train_y, test_x, test_y):
     model = LogisticRegression()
-    model.fit(x, y)
+    model.fit(train_x, train_y)
 
     predictions = model.predict(test_x)
 
-    accuracy = accuracy_score(true_res, predictions)
-    precision = precision_score(true_res, predictions)
-    recall = recall_score(true_res, predictions)
-    f1 = f1_score(true_res, predictions)
+    accuracy = accuracy_score(test_y, predictions)
+    precision = precision_score(test_y, predictions)
+    recall = recall_score(test_y, predictions)
+    f1 = f1_score(test_y, predictions)
 
     print('\n1 - выжил, 0 - погиб')
     print(f"accuracy: {accuracy}")
@@ -59,45 +60,15 @@ def remove_outliers(train_df: pd.DataFrame, test_df: pd.DataFrame, outlier_param
     return train_df, test_df
 
 
-train_dataset = pd.read_csv('train.csv')
-test_dataset = pd.read_csv('test.csv')
-
-y_test = pd.read_csv('test_true.csv')
-
-x_train = train_dataset[['Sex', 'Age']]
-x_test = test_dataset[['Sex', 'Age']]
-
-# Оставил пол так как в первую очередь спасали женщин.
-# Оставил возраст так как в первую очередь спасали детей.
-
-x_test_recoded = pd.get_dummies(x_test['Sex'])
-x_train_recoded = pd.get_dummies(x_train['Sex'])
-
-x_train = pd.concat([x_train, x_train_recoded], axis=1).drop(['Sex', 'female'], axis=1)
-x_test = pd.concat([x_test, x_test_recoded], axis=1).drop(['Sex', 'female'], axis=1)
+orig_dataset = pd.read_csv('train.csv')
 
 
-los_percentage_train = len(x_train.dropna()) * 100 / len(x_train)
-los_percentage_test = len(x_test.dropna()) * 100 / len(x_test)
+orig_dataset_copy = orig_dataset.drop(['Embarked', 'Pclass', 'Sex', 'Name', 'Ticket', 'Cabin'], axis=1).dropna()
 
-print(f'Процент данных, который будет потерян, если просто удалить пропуски:')
-print(f'Test - {los_percentage_test}')
-print(f'Train - {los_percentage_train}')
+x = orig_dataset_copy.drop('Survived', axis=1)
+y = orig_dataset_copy['Survived']
 
-
-x_unprepared_train = train_dataset.copy()
-x_unprepared_train.dropna(inplace=True)
-x_unprepared_train.drop(['Embarked', 'Pclass', 'Sex', 'Name', 'Ticket', 'Cabin', 'Survived'], axis=1, inplace=True)
-
-x_unprepared_test = test_dataset.copy()
-x_unprepared_test.dropna(inplace=True)
-x_unprepared_test.drop(['Embarked', 'Pclass', 'Sex', 'Name', 'Ticket', 'Cabin'], axis=1, inplace=True)
-
-y_unprepared_train = train_dataset[train_dataset['PassengerId'].isin(x_unprepared_train['PassengerId'])]
-y_unprepared_train = y_unprepared_train['Survived']
-
-y_unprepared_test = y_test[y_test['PassengerId'].isin(x_unprepared_test['PassengerId'])]
-y_unprepared_test = y_unprepared_test['Survived']
+x_unprepared_train, x_unprepared_test, y_unprepared_train, y_unprepared_test = train_test_split(x, y, test_size=0.3)
 
 # 'Embarked' - Порт высадки. Удалено, потому что является категориальным значением
 # 'Pclass' - Класс билета. Удалено, потому что является категориальным значением
@@ -107,13 +78,39 @@ y_unprepared_test = y_unprepared_test['Survived']
 # 'Cabin' - Каюта. Удалено, потому что смесь строки и числа
 
 
+orig_dataset_copy = orig_dataset[['Sex', 'Age', 'Survived']]
+
+x = orig_dataset_copy.drop('Survived', axis=1)
+y = orig_dataset_copy['Survived']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
+
+# Оставил пол так как в первую очередь спасали женщин.
+# Оставил возраст так как в первую очередь спасали детей.
+
+los_percentage_train = len(x_train.dropna()) * 100 / len(x_train)
+los_percentage_test = len(x_test.dropna()) * 100 / len(x_test)
+
+
+print(f'Процент данных, который будет потерян, если просто удалить пропуски:')
+print(f'Test - {los_percentage_test}')
+print(f'Train - {los_percentage_train}')
+
+x_test_recoded = pd.get_dummies(x_test['Sex'])
+x_train_recoded = pd.get_dummies(x_train['Sex'])
+
+x_train = pd.concat([x_train, x_train_recoded], axis=1).drop(['Sex', 'female'], axis=1)
+x_test = pd.concat([x_test, x_test_recoded], axis=1).drop(['Sex', 'female'], axis=1)
+
+
 x_train_avg_filled = x_train.copy()
 x_test_avg_filled = x_test.copy()
 
-ages = x_train_avg_filled['Age'].dropna().to_list() + x_test_avg_filled['Age'].dropna().to_list()
+ages = x_train_avg_filled['Age'].dropna().to_list()
+avg_age = sum(ages) / len(ages)
 
-x_train_avg_filled['Age'] = x_train_avg_filled['Age'].fillna(sum(ages) / len(ages))
-x_test_avg_filled['Age'] = x_test_avg_filled['Age'].fillna(sum(ages) / len(ages))
+x_train_avg_filled['Age'] = x_train_avg_filled['Age'].fillna(avg_age)
+x_test_avg_filled['Age'] = x_test_avg_filled['Age'].fillna(avg_age)
 
 
 x_train_rand_filled = x_train.copy()
@@ -123,23 +120,23 @@ x_train_rand_filled['Age'] = put_random(series=x_train_rand_filled['Age'],
                                         min_val=x_train_rand_filled['Age'].min(),
                                         max_val=x_train_rand_filled['Age'].max())
 x_test_rand_filled['Age'] = put_random(series=x_test_rand_filled['Age'],
-                                       min_val=x_test_rand_filled['Age'].min(),
-                                       max_val=x_test_rand_filled['Age'].max())
+                                        min_val=x_train_rand_filled['Age'].min(),
+                                        max_val=x_train_rand_filled['Age'].max())
 
 
-model_operation(x=x_unprepared_train,
-                y=y_unprepared_train,
-                true_res=y_unprepared_test,
+model_operation(train_x=x_unprepared_train,
+                train_y=y_unprepared_train,
+                test_y=y_unprepared_test,
                 test_x=x_unprepared_test)
 
-model_operation(x=x_train_avg_filled,
-                y=train_dataset['Survived'],
-                true_res=y_test['Survived'],
+model_operation(train_x=x_train_avg_filled,
+                train_y=y_train,
+                test_y=y_test,
                 test_x=x_test_avg_filled)
 
-model_operation(x=x_train_rand_filled,
-                y=train_dataset['Survived'],
-                true_res=y_test['Survived'],
+model_operation(train_x=x_train_rand_filled,
+                train_y=y_train,
+                test_y=y_test,
                 test_x=x_test_rand_filled)
 
 
@@ -153,12 +150,12 @@ x_train_rand_filled_without_outliers, x_test_rand_filled_without_outliers = remo
     test_df=x_test_rand_filled,
     outlier_param='Age')
 
-model_operation(x=x_train_avg_filled_without_outliers,
-                y=train_dataset[train_dataset.index.isin(x_train_avg_filled_without_outliers.index)]['Survived'],
-                true_res=y_test[y_test.index.isin(x_test_avg_filled_without_outliers.index)]['Survived'],
+model_operation(train_x=x_train_avg_filled_without_outliers,
+                train_y=orig_dataset[orig_dataset.index.isin(x_train_avg_filled_without_outliers.index)]['Survived'],
+                test_y=orig_dataset[orig_dataset.index.isin(x_test_avg_filled_without_outliers.index)]['Survived'],
                 test_x=x_test_avg_filled_without_outliers)
 
-model_operation(x=x_train_rand_filled_without_outliers,
-                y=train_dataset[train_dataset.index.isin(x_train_rand_filled_without_outliers.index)]['Survived'],
-                true_res=y_test[y_test.index.isin(x_test_rand_filled_without_outliers.index)]['Survived'],
+model_operation(train_x=x_train_rand_filled_without_outliers,
+                train_y=orig_dataset[orig_dataset.index.isin(x_train_rand_filled_without_outliers.index)]['Survived'],
+                test_y=y_test[y_test.index.isin(x_test_rand_filled_without_outliers.index)]['Survived'],
                 test_x=x_test_rand_filled_without_outliers)
