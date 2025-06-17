b4ed4b0320643b8957d9824253b8b8a43d944a6d
ордей абуз <gordlab@mail.ru>
2025-06-17 16:14:05 +1000

    states df

---
+++ task_5/main.py
@@ -0,0 +1,18 @@
+from sklearn.datasets import fetch_california_housing
+import pandas as pd
+
+
+# :Attribute Information:
+#     - MedInc        median income in block group
+#     - HouseAge      median house age in block group
+#     - AveRooms      average number of rooms per household
+#     - AveBedrms     average number of bedrooms per household
+#     - Population    block group population
+#     - AveOccup      average number of household members
+#     - Latitude      block group latitude
+#     - Longitude     block group longitude
+
+
+df = pd.DataFrame(data=fetch_california_housing().data)
+
+
