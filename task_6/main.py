657edbee74c0789f06278e1241980bc6a450544d
ордей абуз <gordlab@mail.ru>
2025-06-25 16:00:21 +1000

    adds DTR model

--- task_6/main.py
+++ task_6/main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
 
 
 def remove_outliers(param: pd.DataFrame):
@@ -44,6 +45,11 @@ Y = dataset['MedHouseVal']
 
 train_x, test_x, train_y, test_y = train_test_split(X, Y, test_size=0.2, random_state=1)
 
+
 model = LinearRegression()
 model.fit(X=train_x, y=train_y)
-score = model.score(X=test_x, y=test_y)
+LR_score = model.score(X=test_x, y=test_y)
+
+model = DecisionTreeRegressor()
+model.fit(X=train_x, y=train_y)
+DTR_score = model.score(X=test_x, y=test_y)
