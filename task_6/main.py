a26b40003499abca45e8684a22de0e55b4da367a
ордей абуз <gordlab@mail.ru>
2025-06-27 19:56:04 +1000

    adds DTR models visualisation

--- task_6/main.py
+++ task_6/main.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.tree import plot_tree
 
 
 def remove_outliers(param: pd.DataFrame):
@@ -24,14 +25,14 @@ def remove_outliers(param: pd.DataFrame):
 
 dataset = pd.read_csv('dataset.csv')
 
-print(f'Кол-во пропусков в каждом признаке: \n{dataset.isnull().sum()}')
-for col in dataset.columns.to_list():
-    plt.figure(figsize=(8, 6))
-    plt.boxplot(dataset[col], vert=False)
-    plt.title(f'Boxplot для признака "{col}"')
-    plt.xlabel('Значения')
-    plt.grid(True, linestyle='--', alpha=0.7)
-    plt.show()
+# print(f'Кол-во пропусков в каждом признаке: \n{dataset.isnull().sum()}')
+# for col in dataset.columns.to_list():
+#     plt.figure(figsize=(8, 6))
+#     plt.boxplot(dataset[col], vert=False)
+#     plt.title(f'Boxplot для признака "{col}"')
+#     plt.xlabel('Значения')
+#     plt.grid(True, linestyle='--', alpha=0.7)
+#     plt.show()
 
 dataset = remove_outliers(dataset)
 
@@ -53,3 +54,16 @@ LR_score = model.score(X=test_x, y=test_y)
 model = DecisionTreeRegressor()
 model.fit(X=train_x, y=train_y)
 DTR_score = model.score(X=test_x, y=test_y)
+
+plt.figure(figsize=(12, 8))
+plot_tree(
+    model,
+    feature_names=X.columns.to_list(),
+    precision=2,
+    filled=True,
+    rounded=True,
+    fontsize=10,
+    max_depth=3
+)
+plt.title("Первые 3 уровня дерева решений")
+plt.show()
