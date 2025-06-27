87e84474e7818c6f2b6ceb5e63f7068300b77816
ордей абуз <gordlab@mail.ru>
2025-06-27 20:11:24 +1000

    adds better DTR

--- task_6/main.py
+++ task_6/main.py
@@ -9,30 +9,30 @@ from sklearn.tree import plot_tree
 
 
 def remove_outliers(param: pd.DataFrame):
-    for col in param.columns.to_list():
-        q1 = np.quantile(param[col], 0.25)
-        q3 = np.quantile(param[col], 0.75)
+    for column in param.columns.to_list():
+        q1 = np.quantile(param[column], 0.25)
+        q3 = np.quantile(param[column], 0.75)
 
         iqr = q3 - q1
 
         lower = q1 - iqr * 1.5
         upper = q3 + iqr * 1.5
 
-        param = param[(param[col] >= lower) & (param[col] <= upper)]
+        param = param[(param[column] >= lower) & (param[column] <= upper)]
 
     return param
 
 
 dataset = pd.read_csv('dataset.csv')
 
-# print(f'Кол-во пропусков в каждом признаке: \n{dataset.isnull().sum()}')
-# for col in dataset.columns.to_list():
-#     plt.figure(figsize=(8, 6))
-#     plt.boxplot(dataset[col], vert=False)
-#     plt.title(f'Boxplot для признака "{col}"')
-#     plt.xlabel('Значения')
-#     plt.grid(True, linestyle='--', alpha=0.7)
-#     plt.show()
+print(f'Кол-во пропусков в каждом признаке: \n{dataset.isnull().sum()}')
+for col in dataset.columns.to_list():
+    plt.figure(figsize=(8, 6))
+    plt.boxplot(dataset[col], vert=False)
+    plt.title(f'Boxplot для признака "{col}"')
+    plt.xlabel('Значения')
+    plt.grid(True, linestyle='--', alpha=0.7)
+    plt.show()
 
 dataset = remove_outliers(dataset)
 
@@ -55,6 +55,7 @@ model = DecisionTreeRegressor()
 model.fit(X=train_x, y=train_y)
 DTR_score = model.score(X=test_x, y=test_y)
 
+
 plt.figure(figsize=(12, 8))
 plot_tree(
     model,
@@ -67,3 +68,13 @@ plot_tree(
 )
 plt.title("Первые 3 уровня дерева решений")
 plt.show()
+
+
+best_DTR_score = \
+    max(
+        [
+            ((model := DecisionTreeRegressor(max_depth=i_depth).fit(X=train_x, y=train_y)).score(X=test_x, y=test_y),
+             i_depth)
+            for i_depth in range(15, 2, -1)
+        ]
+    )[0]
