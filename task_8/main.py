bd2f2b8c4225ed456ed254cf6ec15f6d2733ca23
ордей абуз <gordlab@mail.ru>
2025-07-08 17:35:18 +1000

    removes unnecessary import

--- task_8/main.py
+++ task_8/main.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 
 from matplotlib import pyplot as plt
-import seaborn as sns
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
