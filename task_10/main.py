e38f46380eea870f3faa6cfacaee28a5b609170a
ордей абуз <gordlab@mail.ru>
2025-07-23 15:59:54 +1000

    removes unnecessary imports

--- task_10/main.py
+++ task_10/main.py
@@ -1,20 +1,10 @@
-import matplotlib.pyplot as plt
 import cv2
-import skimage
 from sklearn.cluster import KMeans, DBSCAN, AgglomerativeClustering
 
 
-def draw_picture(image, title, bgr=True):
-    if bgr:
-        b, g, r = cv2.split(image)
-        image = cv2.merge([r, g, b])
-
-    plt.figure(figsize=(7, 5))
-    plt.axis('off')
-    plt.imshow(image)
-    plt.title(title)
-    plt.show()
+image = cv2.imread('image.jpg')
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Конвертация в RGB
+pixels = image.reshape(-1, 3)
 
 
-img = cv2.imread('image.jpg')
-draw_picture(img, 'Оригинальное изображение', bgr=True)
+model = KMeans()
