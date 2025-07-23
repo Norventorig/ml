dc718a4724fcfa6b7a6d238b5a40b434302d9a9e
ордей абуз <gordlab@mail.ru>
2025-07-23 16:23:19 +1000

    adds Kmeans and returns depiction function

--- task_10/main.py
+++ task_10/main.py
@@ -1,5 +1,18 @@
 import cv2
+from matplotlib import pyplot as plt
 from sklearn.cluster import KMeans, DBSCAN, AgglomerativeClustering
+
+
+def draw_picture(image_func, title, bgr=True):
+    if bgr:
+        b, g, r = cv2.split(image_func)
+        image_func = cv2.merge([r, g, b])
+
+    plt.figure(figsize=(7, 5))
+    plt.axis('off')
+    plt.imshow(image_func)
+    plt.title(title)
+    plt.show()
 
 
 image = cv2.imread('image.jpg')
@@ -7,4 +20,12 @@ image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Конвертация в RGB
 pixels = image.reshape(-1, 3)
 
 
-model = KMeans()
+for n in (2, 5, 10, 20):
+    model = KMeans(n_clusters=n, random_state=1, n_init=10)
+
+    labels = model.fit_predict(X=pixels)
+    new_pixels = model.cluster_centers_[labels]
+
+    new_image = new_pixels.reshape(image.shape).astype('uint8')
+
+    draw_picture(image_func=new_image, title=f'Kmeans{n}', bgr=False)
