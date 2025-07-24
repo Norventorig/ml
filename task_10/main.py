82d6b71bc1250a803a5b07ab9012e6aaa380eabe
ордей абуз <gordlab@mail.ru>
2025-07-24 13:48:55 +1000

    adds ssim results

--- task_10/main.py
+++ task_10/main.py
@@ -4,6 +4,14 @@ from matplotlib import pyplot as plt
 from sklearn.cluster import KMeans, DBSCAN, AgglomerativeClustering
 from sklearn.utils import resample
 from scipy.spatial.distance import cdist
+from skimage.metrics import structural_similarity as ssim
+
+
+def calculate_ssim(orig_image, cluster_image):
+    orig_gray = cv2.cvtColor(orig_image, cv2.COLOR_RGB2GRAY)
+    cluster_gray = cv2.cvtColor(cluster_image, cv2.COLOR_RGB2GRAY)
+
+    return ssim(im1=orig_gray, im2=cluster_gray, data_range=255)
 
 
 def draw_picture(image_func, title, bgr=True):
@@ -22,6 +30,7 @@ image = cv2.imread('image.jpg')
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 pixels = image.reshape(-1, 3)
 
+results = []
 
 for n in (2, 5, 10, 20):
     model = KMeans(n_clusters=n, random_state=1, n_init=10)
@@ -31,6 +40,8 @@ for n in (2, 5, 10, 20):
 
     new_image = new_pixels.reshape(image.shape).astype('uint8')
     draw_picture(image_func=new_image, title=f'Kmeans{n}', bgr=False)
+
+    results.append((calculate_ssim(orig_image=image, cluster_image=new_image), f'Kmeans{n}'))
 
 
 model = DBSCAN(eps=3, min_samples=4)
@@ -47,6 +58,8 @@ new_pixels[labels == -1] = [0, 0, 0]
 
 new_image = new_pixels.reshape(image.shape).astype('uint8')
 draw_picture(image_func=new_image, title='DBSCAN', bgr=False)
+
+results.append((calculate_ssim(orig_image=image, cluster_image=new_image), 'DBSCAN'))
 
 
 sample = resample(pixels, n_samples=10000, random_state=0, replace=False) if pixels.shape[0] > 10000 else pixels
@@ -68,4 +81,10 @@ for n in (2, 5, 10, 20):
     new_pixels = centroids[full_labels]
 
     new_image = new_pixels.reshape(image.shape)
-    draw_picture(image_func=new_image, title=f'Agglomerative {n}', bgr=False)
+    draw_picture(image_func=new_image, title=f'AgglomerativeClustering{n}', bgr=False)
+
+    results.append((calculate_ssim(orig_image=image, cluster_image=new_image), f'AgglomerativeClustering{n}'))
+
+
+for i_res in results:
+    print(f'\nИмя модели: {i_res[1]} ---- Результат метрики SSIM: {i_res[0]}')
