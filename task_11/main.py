5d66c8e7721a58ecb729e06300b8f060cda3c121
ордей абуз <gordlab@mail.ru>
2025-08-05 16:42:46 +1000

    adds cross_validation

--- task_11/main.py
+++ task_11/main.py
@@ -1,12 +1,17 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_validate
+from sklearn.linear_model import LogisticRegression
 
 
 dataset = pd.read_csv('dataset.csv')
-dataset = pd.get_dummies(data=dataset)
+dataset = pd.get_dummies(columns=dataset.drop('HeartDisease', axis=1).columns.to_list(), data=dataset)
 
 
 X = dataset.drop('HeartDisease', axis=1)
 y = dataset['HeartDisease']
 
-train_x, train_y, test_x, test_y = train_test_split(X, y, test_size=0.2, random_state=1)
+train_x, test_x, train_y, test_y = train_test_split(X, y, test_size=0.2, random_state=1)
+
+
+res = cross_validate(estimator=LogisticRegression(), y=y, X=X, cv=10, scoring=['accuracy', 'recall', 'precision', 'f1'])
+
