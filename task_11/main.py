547479993149f6b39102ee4a4a0d81a4caad3a8b
ордей абуз <gordlab@mail.ru>
2025-08-05 18:40:20 +1000

    add grid search of superparams

--- task_11/main.py
+++ task_11/main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split, cross_validate
+from sklearn.model_selection import train_test_split, cross_validate, GridSearchCV
 from sklearn.linear_model import LogisticRegression
 
 
@@ -13,5 +13,17 @@ y = dataset['HeartDisease']
 train_x, test_x, train_y, test_y = train_test_split(X, y, test_size=0.2, random_state=1)
 
 
-cv_results = cross_validate(estimator=LogisticRegression(), y=y, X=X, cv=10, scoring=['accuracy', 'recall', 'precision', 'f1'])
+cv_results = cross_validate(estimator=LogisticRegression(), y=y, X=X, cv=10,
+                            scoring=['accuracy', 'recall', 'precision', 'f1'])
+
+
+grid_params = {'C': [0.1, 0.5, 1, 10, 100],
+               'solver': ['liblinear', 'lbfgs', 'saga', 'newton-cg'],
+               'penalty': ['l2', None],
+               'max_iter': [10000, 20000, 50000, 100000]}
+grid_search = GridSearchCV(estimator=LogisticRegression(), param_grid=grid_params, scoring='accuracy')
+
+grid_search.fit(X=train_x, y=train_y)
+gs_params = grid_search.best_params_
+gs_score = grid_search.best_score_
 
