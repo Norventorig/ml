fae48551ff0507f7adee4683b1ad3f0c07b04fc1
ордей абуз <gordlab@mail.ru>
2025-08-10 15:27:35 +1000

    adds mutual_info analysis

--- first_ml_education/lab_work/main.py
+++ first_ml_education/lab_work/main.py
@@ -2,6 +2,7 @@ from imblearn.over_sampling import RandomOverSampler
 from imblearn.under_sampling import RandomUnderSampler
 from sklearn.decomposition import PCA
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import mutual_info_classif
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from ucimlrepo import fetch_ucirepo
@@ -20,42 +21,42 @@ def make_classification_report(x_train, x_test, y_train, y_test):
 
 dataset = fetch_ucirepo(id=40).data.features
 
-data_graph = pd.Series({'Catholic': dataset['religion'][dataset['religion'] == 0].count(),
-                        'Other Christian': dataset['religion'][dataset['religion'] == 1].count(),
-                        'Muslim': dataset['religion'][dataset['religion'] == 2].count(),
-                        'Buddhist': dataset['religion'][dataset['religion'] == 3].count(),
-                        'Hindu': dataset['religion'][dataset['religion'] == 4].count(),
-                        'Ethnic': dataset['religion'][dataset['religion'] == 5].count(),
-                        'Marxist': dataset['religion'][dataset['religion'] == 6].count(),
-                        'Others': dataset['religion'][dataset['religion'] == 7].count()})
+# data_graph = pd.Series({'Catholic': dataset['religion'][dataset['religion'] == 0].count(),
+#                         'Other Christian': dataset['religion'][dataset['religion'] == 1].count(),
+#                         'Muslim': dataset['religion'][dataset['religion'] == 2].count(),
+#                         'Buddhist': dataset['religion'][dataset['religion'] == 3].count(),
+#                         'Hindu': dataset['religion'][dataset['religion'] == 4].count(),
+#                         'Ethnic': dataset['religion'][dataset['religion'] == 5].count(),
+#                         'Marxist': dataset['religion'][dataset['religion'] == 6].count(),
+#                         'Others': dataset['religion'][dataset['religion'] == 7].count()})
+#
+# plt.pie(x=data_graph, labels=data_graph.index)
+# plt.title("distribution of religions")
+# plt.show()
 
-plt.pie(x=data_graph, labels=data_graph.index)
-plt.title("distribution of religions")
-plt.show()
-
-data_graph = pd.crosstab(
-    dataset['religion'].replace({
-        0: 'Catholic',
-        1: 'Other Christian',
-        2: 'Muslim',
-        3: 'Buddhist',
-        4: 'Hindu',
-        5: 'Ethnic',
-        6: 'Marxist',
-        7: 'Others'
-    }),
-    dataset['zone'].replace({
-        1: 'NE',
-        2: 'SE',
-        3: 'SW',
-        4: 'NW'
-    }))
-
-sns.heatmap(data=data_graph, annot=True)
-plt.title('Распределение религий по зонам')
-plt.xlabel('Религия')
-plt.ylabel('Географическая зона')
-plt.show()
+# data_graph = pd.crosstab(
+#     dataset['religion'].replace({
+#         0: 'Catholic',
+#         1: 'Other Christian',
+#         2: 'Muslim',
+#         3: 'Buddhist',
+#         4: 'Hindu',
+#         5: 'Ethnic',
+#         6: 'Marxist',
+#         7: 'Others'
+#     }),
+#     dataset['zone'].replace({
+#         1: 'NE',
+#         2: 'SE',
+#         3: 'SW',
+#         4: 'NW'
+#     }))
+#
+# sns.heatmap(data=data_graph, annot=True)
+# plt.title('Распределение религий по зонам')
+# plt.xlabel('Религия')
+# plt.ylabel('Географическая зона')
+# plt.show()
 
 
 dataset = pd.get_dummies(data=dataset, columns=['zone', 'landmass', 'language', 'mainhue', 'botright', 'topleft'])
@@ -73,12 +74,12 @@ print(f'Классификация при оригинальных данных:
       f'\n{make_classification_report(x_train=train_x, y_train=train_y, x_test=test_x, y_test=test_y)}')
 
 
-data_graph = dataset.corr()['religion'].abs().sort_values(ascending=False).head(6).index
-data_graph = dataset[data_graph].corr()
-
-sns.heatmap(data=data_graph, annot=True)
-plt.title('Correlation matrix')
-plt.show()
+# data_graph = dataset.corr()['religion'].abs().sort_values(ascending=False).head(6).index
+# data_graph = dataset[data_graph].corr()
+#
+# sns.heatmap(data=data_graph, annot=True)
+# plt.title('Correlation matrix')
+# plt.show()
 
 
 # for i_param in X.columns:
@@ -110,3 +111,7 @@ X = pd.DataFrame(data=PCA(n_components=5).fit_transform(X))
 
 print(f'Классификация после уменьшения размерности пространства признаков: '
       f'\n{make_classification_report(x_train=train_x, y_train=train_y, x_test=test_x, y_test=test_y)}')
+
+
+mi = mutual_info_classif(X=X, y=y)
+print(f'Mutual Information признаков с уменьшенной размерностью: {mi}')
