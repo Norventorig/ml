22b815175e5cc85f72898fcf9d94d8ecc56aea58
Norvent <gordlab@mail.ru>
2025-12-15 19:43:21 +1000

    fixes load_train_generator augmentation

--- cv_education/task_2/main.py
+++ cv_education/task_2/main.py
@@ -1,25 +1,31 @@
 import cv2
 from pathlib import Path
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import albumentations as A
+from matplotlib import pyplot as plt
 
-TRAIN_DATAGEN = ImageDataGenerator(rotation_range=180,
-                                   width_shift_range=0.2,
-                                   height_shift_range=0.2,
-                                   zoom_range=0.2,
-                                   horizontal_flip=True,
-                                   vertical_flip=True,
-                                   shear_range=0.1,
-                                   channel_shift_range=10.0)
+TRAIN_DATAGEN = A.Compose([A.HorizontalFlip(p=0.25),
+                           A.VerticalFlip(p=0.4),
+                           A.ShiftScaleRotate(shift_limit=0.05,
+                                              scale_limit=0.1,
+                                              rotate_limit=30,
+                                              p=0.5),
+                           A.ColorJitter(brightness=0.2,
+                                         contrast=0.2,
+                                         saturation=0.2,
+                                         hue=0.1,
+                                         p=0.3)])
 
 
-def load_train_generator(path, img_size, augmentator):
+def load_train_generator(path: str, img_size: tuple, augmentator):
     directory = Path(path)
+
     for i_path in directory.iterdir():
         img = cv2.imread(i_path, cv2.IMREAD_UNCHANGED)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         img = cv2.resize(img, img_size)
-        augmentator.flow(img.reshape((1, *img_size, 3)), batch_size=1)
+        new_img = augmentator(image=img)
 
-        yield img
+        yield new_img['image']
 
 
 load_train_gen = load_train_generator(
