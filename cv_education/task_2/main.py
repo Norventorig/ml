49103cbfc7ae23b7bf701bcd5aed13843010ef75
wdwdwdw <yshwh@mail.ru>
2025-12-19 18:35:22 +1000

    adds augmentator

--- cv_education/task_2/main.py
+++ cv_education/task_2/main.py
@@ -7,3 +7,22 @@ from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 from tensorflow.keras.layers import Dense, Dropout, GlobalAveragePooling2D
 from tensorflow.keras import Model
 from tensorflow.keras.applications import vgg16
+
+
+TRAIN_DATAGEN = A.Compose([
+    A.HorizontalFlip(p=0.25),
+    A.ShiftScaleRotate(
+        shift_limit=0.05,
+        scale_limit=0.1,
+        rotate_limit=30,
+        p=0.5
+    ),
+    A.ColorJitter(
+        brightness=0.2,
+        contrast=0.2,
+        saturation=0.2,
+        hue=0.1,
+        p=0.3
+    )
+])
+
