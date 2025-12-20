c4ddca0630f8c604996d5685544c17d68bf41d55
wdwdwdw <yshwh@mail.ru>
2025-12-20 19:20:10 +1000

    completes model handler

---
+++ cv_education/task_2/predict.py
@@ -0,0 +1,32 @@
+from tensorflow.keras.models import load_model
+import cv2
+from pathlib import Path
+import numpy as np
+from tensorflow.keras.applications.vgg16 import preprocess_input
+import pandas as pd
+
+
+def prepare(path):
+    img = cv2.imread(str(path))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.resize(img, (224, 224))
+    img = preprocess_input(img)
+    img = np.expand_dims(img, 0)
+
+    return img
+
+
+model = load_model("model.keras", compile=False)
+labels = ('cat', 'dog')
+data = {'id': [], 'label': []}
+
+for i_path in Path(rf'C:\Users\123\Downloads\datasets\test').iterdir():
+    value = prepare(path=i_path)
+
+    value_id = str(i_path.name)[4:-4]
+    predicted_label= labels[int(model.predict(value)[0][0])]
+
+    data['label'].append(predicted_label)
+    data['id'].append(value_id)
+
+pd.DataFrame(data=data).to_csv('result.csv', index=False)
