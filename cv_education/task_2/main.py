27b6459b5e6f7ed97c3003bdfbc15b516fc2cd14
wdwdwdw <yshwh@mail.ru>
2025-12-20 13:03:23 +1000

    improves load_train_images

--- cv_education/task_2/main.py
+++ cv_education/task_2/main.py
@@ -2,6 +2,7 @@ import cv2
 from pathlib import Path
 import albumentations as A
 import numpy as np
+import random
 
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 from tensorflow.keras.layers import Dense, Dropout, GlobalAveragePooling2D
@@ -27,33 +28,40 @@ TRAIN_DATAGEN = A.Compose([
 ])
 
 
-def load_images(path, img_size, label_name='cat'):
+def load_train_images(path, img_size: tuple, labels: tuple):
     x, y = [], []
     directory = Path(path)
 
-    for img_path in directory.iterdir():
-        label = int(label_name in img_path.name)
+    for subdir in directory.iterdir():
+        label = labels.index(subdir.name)
 
-        img = cv2.imread(str(img_path))
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        img = cv2.resize(img, img_size)
+        for img_path in subdir.iterdir():
+            img = cv2.imread(str(img_path))
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            img = cv2.resize(img, img_size)
 
-        img = vgg16.preprocess_input(img)
+            aug_img = TRAIN_DATAGEN(image=img)['image']
 
-        x.append(img)
-        y.append(label)
+            img = vgg16.preprocess_input(img)
+            aug_img = vgg16.preprocess_input(aug_img)
 
-        aug_img = TRAIN_DATAGEN(image=img)['image']
-        x.append(aug_img)
-        y.append(label)
+            x.append(img)
+            y.append(label)
+
+            x.append(aug_img)
+            y.append(label)
+
+    combined = list(zip(x, y))
+    random.shuffle(combined)
+    x, y = zip(*combined)
 
     return np.array(x), np.array(y)
 
 
-x_train, y_train = load_images(
-    path=r"C:\Users\123\Downloads\train",
-    img_size=(224, 224)
-)
+x_train, y_train = load_train_images(
+    path=r"C:\Users\123\Downloads\datasets\train",
+    img_size=(224, 224),
+    labels=("cat", "dog"))
 
 
 base_model = vgg16.VGG16(
@@ -65,11 +73,11 @@ base_model = vgg16.VGG16(
 for layer in base_model.layers:
     layer.trainable = False
 
-x = base_model.get_layer('block5_conv3').output
-x = GlobalAveragePooling2D()(x)
-x = Dense(1024, activation='relu')(x)
-x = Dropout(0.5)(x)
-outputs = Dense(1, activation='sigmoid')(x)
+outputs = base_model.get_layer('block5_conv3').output
+outputs = GlobalAveragePooling2D()(outputs)
+outputs = Dense(1024, activation='relu')(outputs)
+outputs = Dropout(0.5)(outputs)
+outputs = Dense(1, activation='sigmoid')(outputs)
 
 model = Model(inputs=base_model.input, outputs=outputs)
 
