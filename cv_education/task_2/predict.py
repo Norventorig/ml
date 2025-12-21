07d1df69572bcdc384e221d414e57064536fa985
wdwdwdw <yshwh@mail.ru>
2025-12-21 17:44:10 +1000

    changes data in label

--- cv_education/task_2/predict.py
+++ cv_education/task_2/predict.py
@@ -33,9 +33,11 @@ for n_iter, i_path in enumerate(paths):
     value = prepare(path=i_path)
 
     value_id = str(i_path.name)[:-4]
-    predicted_label= int(model.predict(value)[0][0])
 
-    data['label'].append(predicted_label)
+    prediction = model.predict(value)
+    prediction = float(prediction[0][0])
+
+    data['label'].append(prediction)
     data['id'].append(value_id)
 
 pd.DataFrame(data=data).to_csv('result.csv', index=False)
